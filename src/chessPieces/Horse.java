fe16558af6c81365139695d947a347797b29ea9f
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-10-05 16:51:44 +0300

    Added horse piece with minimal logic

--- src/chessPieces/Horse.java
+++ src/chessPieces/Horse.java
@@ -1,4 +1,25 @@
 package chessPieces;
 
-public class Horse {
+import chessPieces.resources.Color;
+import chessPieces.resources.Type;
+
+public class Horse extends ChessPiece {
+
+    public Horse(int x, int y, Color color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.type = Type.ELEPHANT;
+    }
+
+    @Override
+    public boolean move(int xGoal, int yGoal) {
+        if (((Math.abs(xGoal - this.x) == 2) && (Math.abs(yGoal - this.y) == 1)) ||
+                ((Math.abs(xGoal - this.x) == 1) && (Math.abs(yGoal - this.y) == 2))) {
+            this.x = xGoal;
+            this.y = yGoal;
+            return true;
+        } else return false;
+    }
+
 }
