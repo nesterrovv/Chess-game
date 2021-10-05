280f4048207b5b8caa1f337eef5f8a9c13693e2d
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-10-05 14:29:53 +0300

    Adeed pawn piece with minimal logic

--- src/chessPieces/ChessPiece.java
+++ src/chessPieces/ChessPiece.java
@@ -5,16 +5,16 @@ import chessPieces.resources.Type;
 
 public abstract class ChessPiece {
 
-    private int x;
-    private int y;
-    private Color color;
-    private Type type;
+    protected int x;
+    protected int y;
+    protected Color color;
+    protected Type type;
 
-    public boolean move() {
+    public boolean move(int xGoal, int yGoal) {
         return true; // if movement is available, otherwise - false
     }
 
-    public boolean take() {
+    public boolean take(int xGoal, int yGoal) {
         return true; // if taking is available, otherwise - false
     }
 
---
+++ src/chessPieces/Pawn.java
@@ -0,0 +1,39 @@
+package chessPieces;
+
+import chessPieces.resources.Color;
+import chessPieces.resources.Type;
+
+public class Pawn extends ChessPiece {
+
+    private boolean firstMovement = true;
+
+    public Pawn(int x, int y, Color color, Type type) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.type = Type.PAWN;
+    }
+
+    @Override
+    public boolean move(int xGoal, int yGoal) {
+        if (firstMovement) {
+            if ((yGoal - this.y == 1 || yGoal - this.y == 2) && xGoal - this.x == 0) {
+                this.firstMovement = false;
+                this.y = yGoal; // movement
+                return true;
+            } else return false;
+        } else {
+            return yGoal - this.y == 1 && xGoal - this.x == 0;
+        }
+    }
+
+    @Override
+    public boolean take(int xGoal, int yGoal) {
+        if (xGoal - this.x == 1 && yGoal - this.y == 1) {
+            this.x = xGoal;
+            this.y = yGoal;
+            return true;
+        } else return false;
+    }
+
+}
